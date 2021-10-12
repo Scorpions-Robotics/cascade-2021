960caf46c310f19f0b5646f9f0a5a7a30e2679be
egeakman <egeakmanegeakman@hotmail.com>
2021-10-12 18:30:23 +0300

    Initial commit.

---
+++ crawler.py
@@ -0,0 +1,9 @@
+from icrawler.builtin import BingImageCrawler
+
+classes = ["robots", "ball", "Human faces"]
+number = 30
+for c in classes:
+    bing_crawler = BingImageCrawler(
+        storage={"root_dir": f'data-sets/n/{c.replace(" ",".")}'}
+    )
+    bing_crawler.crawl(keyword=c, filters=None, max_num=number, offset=0)
